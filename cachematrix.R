2bbf9b37a04842f374cd4b90816deae06d17e054
infinitooples <43046089+infinitooples@users.noreply.github.com>
2018-09-16 18:38:42 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions use the built in solve() function to calculate the inverse of a matrix, check if the inverse of that matrix
+##was already solved, and then return a cached value of the inverse if the matrix was in fact already solved.  The functions
+##should be used together so that if M is the matrix inverse.M <- cacheSolve(makeCacheMatrix(M))
 
-## Write a short comment describing this function
+##makeCacheMatrix takes a matrix as an input and creates four utility functions as an output.  
+##These functions are used by cacheSolve.  set() creates a value for the matrix, get() returns the value for the matrix,
+##setinv() creates a value for the inverse of the matrix, and getinv() returns the value of the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+                setinv = setinv,
+                getinv = getinv)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
